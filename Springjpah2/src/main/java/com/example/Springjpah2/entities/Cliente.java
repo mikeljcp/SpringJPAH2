3c3666a1b26bb680c445e18fd79db261d9343dcc
Mikel J <mikeljcp@gmail.com>
2023-04-16 17:47:49 -0300

    Classes separadas por paquetes

--- Springjpah2/src/main/java/com/example/Springjpah2/entities/Cliente.java
+++ Springjpah2/src/main/java/com/example/Springjpah2/entities/Cliente.java
@@ -1,4 +1,4 @@
-package com.example.Springjpah2;
+package com.example.Springjpah2.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- Springjpah2/src/main/java/com/example/Springjpah2/repository/ClienteRepository.java
+++ Springjpah2/src/main/java/com/example/Springjpah2/repository/ClienteRepository.java
@@ -1,5 +1,6 @@
-package com.example.Springjpah2;
+package com.example.Springjpah2.repository;
 
+import com.example.Springjpah2.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
