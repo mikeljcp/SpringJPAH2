013540f9f949ffe6b56e8ab53687f7d98c41d1e0
Mikel J <mikeljcp@gmail.com>
2023-04-16 11:33:02 -0300

    Proyecto inicial Spring

---
+++ Springjpah2/src/test/java/com/example/Springjpah2/Springjpah2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springjpah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springjpah2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
